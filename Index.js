ab3cdf8d830e822dd7e0c44a05320f7e1541d0ce
PavanManojk <143111076+PavanManojk@users.noreply.github.com>
2023-09-03 01:00:34 +0530

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,12 @@
+
+
+function Hello()
+  {
+    c=document.getElementById("hello")
+    c.innerText="hello"
+  }
+function jane()
+  {
+    
+    document.getElementById("jane").innerText="Jane"
+  }
